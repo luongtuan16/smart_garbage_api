1c27e42d50d1be4251a7b830a906549081653f36
luongtuan16 <luongtuan16@gmail.com>
2023-02-27 00:19:39 +0700

    Update: cron job take out trash

--- utils/cron.ts
+++ utils/cron.ts
@@ -5,7 +5,7 @@ import { BinModel } from "../mongo/Bin";
 import { TrashModel } from "../mongo/Trash";
 import { datesAreOnSameDay } from "./utils";
 
-export const jobTakeOutTrash = cron.schedule('*/5 * * * *', async () => {//every 5 min
+export const jobTakeOutTrash = cron.schedule('*/5 * * *', async () => {//every 5 hours
     //const numBin = Math.round(Math.random() * 3) + 1;
     const numBin = 1;
     const getRandomTrash = () => {
